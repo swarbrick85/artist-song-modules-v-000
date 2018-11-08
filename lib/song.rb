37c14ef39b4176f35e72eef8fccbfa4d56cc9d3b
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-11-08 18:38:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,16 +9,16 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@all = []
+  @@songs = []
 
   def initialize
-    @@all << self
+    @@songs << self
   end
 
   
 
   def self.all
-    @@all
+    @@songs 
   end
 
   
