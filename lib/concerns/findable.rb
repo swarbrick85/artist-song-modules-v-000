3f88c4625aeff180c1d27406fdf4942b1c751be6
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-11-08 18:31:48 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -5,13 +5,5 @@ module Findable
   def find_by_name(name)
     self.all.detect{|a| a.name == name}
   end
-  
-#  def find_by_name(name)
-#    @@songs.detect{|a| a.name == name}
-#  end
-  
- 
 
-  
-  
 end 
